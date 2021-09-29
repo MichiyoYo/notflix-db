e91e4e777d3e1b705e17cffcee0b7c2ed734d186
Cristina Lester <cristina.deelester@gmail.com>
2021-09-28 17:46:35 -0700

    added login form

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,7 +1,37 @@
 import React from "react";
+import { Form, Button, Col, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function Login(props) {
-  return <div>Login</div>;
+  return (
+    <Row className="justify-content-center">
+      <Col lg={6} md={8} sm={12} className="login-wrapper">
+        <h2>Login</h2>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Username</Form.Label>
+            <Form.Control type="text" placeholder="Your Username" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Your Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Remember me" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+        <p className="text-muted mt-20 mb-0">
+          No account yet?{" "}
+          <Link to="/register">
+            <Button variant="link">Register</Button>
+          </Link>
+        </p>
+      </Col>
+    </Row>
+  );
 }
 
 export default Login;
